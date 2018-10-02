f266e557e437fa865bc5fcf509bdc7eed43a8333
AdRusinek <rusinekadrian19@gmail.com>
2018-10-02 22:14:49 +0200

    refactoring to common crud interface. closes #11

--- sfg-pet--clinic/pet--clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet--clinic/pet--clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.springframework.data.repository.CrudRepository;
+
 import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
@@ -13,4 +15,5 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
 }
