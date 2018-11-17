fc3967e52b96a9b93021f1f294af73375d1d736e
AdRusinek <rusinekadrian19@gmail.com>
2018-11-17 12:59:05 +0100

    Added Owner list and updated Controller. Closes #16

--- sfg-pet--clinic/pet--clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet--clinic/pet--clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // dzieki temu RequestMapping nad klasa n
@@ -10,9 +12,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
-    // ten pusty String jest potrzebny
+    private final OwnerService ownerService;
+
+    // constructor injection is the preffered way working with injected properties.
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+    /**
+     * this empty String is needed.
+     * addAttribute takes "owner"  this is gonna be the name of the property inside the model
+     * and it will be all owners from the findAll() method
+     * ownerService.findAll() this is gonna give us a Set that we will be able to iterate over
+     * @param model Spring MVC is automatically going to add a model
+     *              when it calls this method to list
+     * @return  to thymeleaf template
+     */
     @RequestMapping({"", "/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
